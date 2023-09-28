62796345b16bf41d93c6becc2c806fad78eafe1f
Amaury Prieto <amaury@olivetreeholdings.com>
2023-09-28 08:02:32 -0500

    team

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,7 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+import Lightbox from 'bs5-lightbox';
+
+document.querySelectorAll('.my-lightbox-toggle').forEach(el => el.addEventListener('click', Lightbox.initialize));
